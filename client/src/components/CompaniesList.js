7a9a459d7444cbbc60285735e0eb4df230735f3c
Fintan Hahnefeld <fintan22@gmail.com>
2021-05-19 11:30:13 +0200

    banner-add-2

--- client/src/components/CompaniesList.js
+++ client/src/components/CompaniesList.js
@@ -45,16 +45,3 @@ export class CompaniesList extends Component {
 }
 
 export default CompaniesList
-
-//THIS IS THE LOGIC TO CALCULATE THE TOP 100
-// const top100 = this.props.companies.sort((a, b) => {
-//   let aAvg = (a.diversity_total + a.growth_score + a.opportunity_score) / 3;
-//   let bAvg = (b.diversity_total + b.growth_score + b.opportunity_score) / 3;
-//   if (aAvg > bAvg) {
-//     return -1;
-//   }
-//   if (aAvg < bAvg) {
-//     return 1;
-//   }
-//   return 0;      
-// }).splice(0, 10);
