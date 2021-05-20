1527cc02a52be629dfb3286958f345077f65d082
Rimbert Richter <rimbert.richter@gmail.com>
2021-05-21 00:58:58 +0200

    mogobd_uri

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ require('./config')(app)
 
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
-const DB_URL = 'mongodb://localhost/projector'
+DB_URL = process.env.MONGODB_URI
 
 app.use(
   session({
